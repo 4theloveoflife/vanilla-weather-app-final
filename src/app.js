9d0aaa15526414e36ffbecc516af297d0bd20eaf
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-08-08 21:43:42 +0900

    Update app.js

--- src/app.js
+++ src/app.js
@@ -56,7 +56,7 @@ function displayForecast(response){
 function getForecast (coordinates){
   console.log(coordinates);
   let apiKey="eada887ccbd6462c016357c4150a3af7";
-  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey} & units=metric`;
+  let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 

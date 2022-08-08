5f0aae1760ad98117f836d7ca53c35b90660bcdf
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-08-08 20:35:05 +0900

    get coordinates from apiUrl
    display forecast after getting coordinates  from lat and lon in java script

--- src/app.js
+++ src/app.js
@@ -23,11 +23,13 @@ function formatDate(timestamp){
 }
 
 function displayForecast(response){
+    console.log (response.data);
     let forecastElement=document.querySelector ("#forecast");
 
-       let forecastHTML=`<div class= "row">`;
-       let days=["Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
-       days.forEach (function (day) {
+           let days=["Tuesday","Wednesday", "Thursday", "Friday", "Saturday"];
+       
+           let forecastHTML=`<div class= "row">`;
+          days.forEach (function (day) {
                 forecastHTML= 
      forecastHTML +
       `
@@ -48,6 +50,14 @@ function displayForecast(response){
     });
      forecastHTML=forecastHTML + `</div>`;
      forecastElement.innerHTML=forecastHTML;
+}
+
+function getForecast (coordinates){
+  console.log(coordinates);
+  let apiKey="eada887ccbd6462c016357c4150a3af7";
+  let apiUrl=`https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey} & units=metric`;
+console.log (apiUrl);
+axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response){
@@ -72,6 +82,10 @@ iconElement.setAttribute(
 `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
 );
 iconElement.setAttribute("alt", response.data.weather[0].description);
+
+getForecast(response.data.coord);
+
+
 }
 
 function search (city) {

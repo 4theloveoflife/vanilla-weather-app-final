3b66e839b7812b022ddc07e9424579035f27b68c
4theloveoflife <103428607+4theloveoflife@users.noreply.github.com>
2022-08-10 01:11:23 +0900

    fixed search and Current button
    added javascript for current location and search buttons

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp){
   let date=new Date(timestamp);
   let hours=date.getHours();
     if (hours < 10){
-    hours=`0 ${hours}`;
+    hours=`0${hours}`;
   }
   let minutes=date.getMinutes();
   if (minutes < 10){
-    minutes=`0 ${minutes}`;
+    minutes=`0${minutes}`;
   }
 
   let days=[
@@ -19,18 +19,15 @@ function formatDate(timestamp){
   "Saturday"
 ];
   let day=days[date.getDay()];
-    return `${day} ${hours}:${minutes}`;
+    return `${day}${hours}:${minutes}`;
 }
 function formatDay (timestamp) {
  let date= new Date (timestamp * 1000);
  let day =date.getDay();
- let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-
-
+ let days= ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 return  days [day];
 }
-
 
 function displayForecast(response){
     let forecast= response.data.daily;
@@ -94,11 +91,9 @@ iconElement.setAttribute(
 iconElement.setAttribute("alt", response.data.weather[0].description);
 
 getForecast(response.data.coord);
-
-
 }
 
-function search (city) {
+function searchCity (city) {
 let apiKey= "eada887ccbd6462c016357c4150a3af7";
 let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
@@ -107,7 +102,18 @@ axios.get(apiUrl).then(displayTemperature);
 function handleSubmit(event){
     event.preventDefault();
     let searchInputElement= document.querySelector("#search-input");
-    search(searchInputElement.value);
+    searchCity(searchInputElement.value);
+}
+
+function searchLocation(position) {
+  let apiKey = "eada887ccbd6462c016357c4150a3af7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 function displayFahrenheitTemperature(event){
@@ -133,6 +139,9 @@ let celsiusTemperature= null;
 let form= document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
 let fahrenheitLink=document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
@@ -140,5 +149,5 @@ let celsiusLink=document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
-search ("Tokyo");
+searchCity ("Tokyo");
 displayForecast;
